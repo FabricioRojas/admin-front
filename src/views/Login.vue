bb1f01660a45e5ea03ee93738cc07e62b82e9bfa
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2020-06-21 12:50:03 +0100

    Minor fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,88 +1,88 @@
 <template>
   <div class="wrapper">
-      <div class="section page-header header-filter" :style="headerStyle">
-        <div class="container">
-          <div class="md-layout">
-            <div
-              class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
-            >
+    <div class="section page-header header-filter" :style="headerStyle">
+      <div class="container">
+        <div class="md-layout">
+          <div
+            class="md-layout-item md-size-33 md-small-size-66 md-xsmall-size-100 md-medium-size-40 mx-auto"
+          >
             <form @submit.prevent="formSubmit">
               <!-- <login-card header-color="green"> -->
-                <h4 slot="title" class="card-title">Login</h4>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>face</md-icon>
-                  <label>First Name...</label>
-                  <md-input v-model="firstname"></md-input>
-                </md-field>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>email</md-icon>
-                  <label>Email...</label>
-                  <md-input v-model="email" type="email"></md-input>
-                </md-field>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>lock_outline</md-icon>
-                  <label>Password...</label>
-                  <md-input v-model="password" type="password"></md-input>
-                </md-field>
-                <button ><md-button slot="footer" class="md-simple md-success md-lg">
-                  Logueame
-                </md-button></button>
+              <h4 slot="title" class="card-title">Login</h4>
+              <md-field class="md-form-group" slot="inputs">
+                <md-icon>face</md-icon>
+                <label>First Name...</label>
+                <md-input v-model="firstname"></md-input>
+              </md-field>
+              <md-field class="md-form-group" slot="inputs">
+                <md-icon>email</md-icon>
+                <label>Email...</label>
+                <md-input v-model="email" type="email"></md-input>
+              </md-field>
+              <md-field class="md-form-group" slot="inputs">
+                <md-icon>lock_outline</md-icon>
+                <label>Password...</label>
+                <md-input v-model="password" type="password"></md-input>
+              </md-field>
+              <button>
+                <md-button slot="footer" class="md-simple md-success md-lg">Logueame</md-button>
+              </button>
               <!-- </login-card> -->
             </form>
-            </div>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // import { LoginCard } from '../components/LoginCard'
-import loginController from '../controllers/login'
+import loginController from "../controllers/login";
 
 export default {
   components: {
     // LoginCard
   },
-  bodyClass: 'login-page',
-  data () {
+  bodyClass: "login-page",
+  data() {
     return {
       firstname: null,
       email: null,
       password: null
-    }
+    };
   },
-  created () {
-    loginController.get()
+  created() {
+    loginController.get();
   },
   props: {
     header: {
       type: String,
-      default: 'black'
+      default: "black"
     }
   },
   methods: {
-    formSubmit () {
-      console.log('submit')
+    formSubmit() {
+      console.log("submit");
       loginController.register({
         username: this.email,
         password: this.password
-      })
+      });
 
       loginController.login({
         username: this.email,
         password: this.password
-      })
+      });
     }
   },
   computed: {
-    headerStyle () {
+    headerStyle() {
       return {
         backgroundImage: `url(${this.header})`
-      }
+      };
     }
   }
-}
+};
 </script>
 
 <style lang="css"></style>
