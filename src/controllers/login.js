ef3537261d1d753c8682fe9a19d0a158ea8d64c6
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2020-06-21 12:41:07 +0100

    Removed some unuseful tailing spaces

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,69 +1,45 @@
 import axios from 'axios'
 
 export default {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-  get () {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      url: 'http://192.168.0.23:3001/'
+    get() {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            url: 'http://192.168.0.23:3001/'
+        }
+        axios(options).then(function (response) {
+            console.log('response', response.data)
+        })
+    },
+    register(data) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            data: data,
+            url: 'http://192.168.0.23:3001/user'
+        }
+        axios(options).then(function (response) {
+            console.log('response', response.data)
+        })
+    },
+    login(data) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            },
+            data: data,
+            url: 'http://192.168.0.23:3001/login'
+        }
+        axios(options).then(function (response) {
+            console.log('response', response.data)
+        })
     }
-    axios(options).then(function (response) {
-      console.log('response', response.data)
-    })
-  },
-  register (data) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      data: data,
-      url: 'http://192.168.0.23:3001/user'
-    }
-    axios(options).then(function (response) {
-      console.log('response', response.data)
-    })
-  },
-  login (data) {
-    const options = {
-      method: 'POST',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      data: data,
-      url: 'http://192.168.0.23:3001/login'
-    }
-    axios(options).then(function (response) {
-      console.log('response', response.data)
-    })
-  }
 }
